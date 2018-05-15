760b60e211c8e1cf80737176acc2448047d2e23d
ltreum <ltreumer@web.de>
2018-05-15 12:11:21 +0200

    Revert "-- update"
    This reverts commit 27486ade2c0cd12dff1f7173c59a579b14d31de7.

--- htdocs/useraccount.php
+++ htdocs/useraccount.php
@@ -43,6 +43,30 @@ include_once 'D:/geo406/htdocs/php_includes/db_connect.inc.php';
         </form>
     </div>
 
+    <?php
+    /*
+      $data = "fargo";
+
+      $sql = "SELECT * FROM person WHERE login=?;";
+      $stmt = mysqli_stmt_init($conn);
+      if (!mysqli_stmt_prepare($stmt, $sql)) {
+      echo "SQL statement failed";
+      } else {
+      //bind parameters to the placeholder
+      mysqli_stmt_bind_param($stmt, 's', $data);
+      // run parameters inside database
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      while ($row = mysqli_fetch_assoc($result)) {
+      echo $row['login'] . "<br>";
+      }
+      }
+      ?>
+
+      <p></p>
+
+     */
+    ?>
 
 
 </section>
