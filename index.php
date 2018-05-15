e5a128060b9e0750080d2451a816dbf67a04a480
EricKrg <eric.krueger@uni-jena.de>
2018-05-15 15:52:21 +0200

    added links to new functions

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ include_once 'header_new.php';
             ?>  <a href="fileupload.php" class="w3-bar-item w3-button w3-padding-large w3-black">Upload Data</a>
             <a href="meta_explorer.php" class="w3-bar-item w3-button w3-padding-large w3-black">edit Meta</a>
             <a href="delete.php" class="w3-bar-item w3-button w3-padding-large w3-black">Delete Data</a>
-            <a href="visual.php" class="w3-bar-item w3-button w3-padding-large w3-black">Visualize Data</a>
+            <a href="visual.php" class="w3-bar-item w3-button w3-padding-large w3-black">Analyze Data</a>
+            <a href="download.php" class="w3-bar-item w3-button w3-padding-large w3-black">Data download</a>
                     <?php
         } else {
             echo "Please sign up or log in you account";
