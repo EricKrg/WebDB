dff7cebdd3490b95545bc59cc3427a8095784d6e
EricKrg <eric.krueger@uni-jena.de>
2018-05-18 11:15:56 +0200

    divided calc. for viusal and visual output

--- visual.php
+++ visual.php
@@ -1,14 +1,12 @@
-
 <?php
 include_once 'header_new.php';
 if (isset($_SESSION['u_id'])) {
 ?>
 <body>
-
     <header class="w3-container w3-green w3-center w3-medium" style="padding:20px 16px">
         <h1 class="w3-margin w3-jumbo"><font size="6">Data Analysis</h1></font>
     </header>
-    <form class="w3-container w3-center" method="post" action="/php_includes/visual.inc.php" name="form">  
+    <form class="w3-container w3-center" method="post" action="/php_includes/visual.display.php" name="form">  
         <select class="w3-button w3-grey w3-medium w3-margin-top" name="visual_file"><p>
             <option class="w3-button w3-red w3-medium w3-margin-top" value="select" selected="selected">Select data</option>
             <?php
