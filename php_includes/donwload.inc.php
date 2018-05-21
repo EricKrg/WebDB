32ee98b5c03683cb40181768a8ada15dfdfb2c37
Eric Krueger <eric.krueger@uni-jena.de>
2018-05-21 21:28:18 +0200

    added return button

--- php_includes/donwload.inc.php
+++ php_includes/donwload.inc.php
@@ -1,4 +1,5 @@
 <?php
+include_once '../header_new.php';
 //donwload function
 function makeDownload($file, $dir, $type) {
     
@@ -15,8 +16,11 @@ $type = 'txt';
 
     if(file_exists ($dir.$_POST['file_name']))     { //checks if file is present - actually obsolet but nice to have
         makeDownload($_POST['file_name'], $dir, $type);
+    } else { ?>
+            <div class="w3-container w3-red w3-left">No File selected </div>
+                        <?php echo '<form action = "../download.php" method = "POST">' ?>
+                    <button class ="w3-button w3-grey " type = "submit" name = "submit">Back</button>
+    <?php
     }
-        
-
-
-?>
+    include_once '../footer.php';
+    ?>
