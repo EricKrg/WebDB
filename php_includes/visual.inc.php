07fb7af7fbf59ce912de55ae46fd1cfe111d2f24
Eric Krueger <eric.krueger@uni-jena.de>
2018-05-21 18:41:52 +0200

    seperated stat. functions

--- php_includes/visual.inc.php
+++ php_includes/visual.inc.php
@@ -28,39 +28,9 @@ include_once '../jpgraph-4.2.0/src/jpgraph_legend.inc.php';
     $type_d = mysqli_fetch_assoc($type);
     $data_type =  $type_d['data_type']; 
 
-//stat functions----------------------------------------------------------------
-    function variance($array,$mean){
-        foreach($array as $item){
-            $tmp[] = pow(($item - $mean),2);
-        }
-        $s = array_sum($tmp)/count($array);
-        return $s;
-    }
-    function quan($array, $x){
-        sort($array);
-        $length= count($array); 
-        $q1 = $length * $x;  
-        return $array[round($q1)-1];      
-  }
-    function Stand_Deviation($arr)
-    {
-        $num_of_elements = count($arr);
-         
-        $variance = 0.0;
-         
-                // calculating mean using array_sum() method
-        $average = array_sum($arr)/$num_of_elements;
-         
-        foreach($arr as $i)
-        {
-            // sum of squares of differences between 
-                        // all numbers and means.
-            $variance += pow(($i - $average), 2);
-        }
-         
-        return (float)sqrt($variance/$num_of_elements);
-    }
+
     // stat values--------------------------------------------------------------
+    include 'stats.inc.php'; #stat functions 
     $a = array_filter($ydata);
     
     $mean = array_sum($a)/count($a);
