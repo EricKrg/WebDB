320d3b69915c678c522b3a7010f970b1058ad9b1
Eric Krueger <eric.krueger@uni-jena.de>
2018-05-21 21:14:52 +0200

    chnaged to mysql_real_ecs_string

--- php_includes/meta_explorer_update.php
+++ php_includes/meta_explorer_update.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../header_new.php';
+include 'db_connect.inc.php';
 // Meta txt edit and DB entry - similar to upload
 ?>
 
@@ -12,14 +13,14 @@ include_once '../header_new.php';
                     
     <?php
             // Meta file change
-            $save =  $_POST['file_name'];
+            $save = mysqli_real_escape_string($conn, $_POST['file_name']);
             $fname_m = substr($save, 5);
             $meta_file = fopen($save , "w");
             $var = array('DataT','Descr','rstat','rdesc','start','enddt','naval',
                         'tstep','respo','metad', 'sname','River', 'Latcr', 'Loncr','utmzn','eleva',
                         'yearE','yearC','rarea','statd','respp');
             foreach ($var as $value){
-                $txt = $value.": ".$_POST[$value]."\n";
+                $txt = $value.": ".mysqli_real_escape_string($conn, $_POST[$value])."\n";
                 fwrite($meta_file, $txt);
                 $meta[] = substr($txt,6);
                 }
@@ -30,7 +31,7 @@ include_once '../header_new.php';
             $row_val = "$fname_m', '".$row_val;
             
             // DB entry 
-            include 'db_connect.inc.php';
+            
             mysqli_query($conn,"DELETE FROM meta_data WHERE `Data_name` = '$fname_m'");
             $meta_update = "INSERT INTO meta_data ($col_var) VALUES ('$row_val')";
             mysqli_query($conn, $meta_update);
