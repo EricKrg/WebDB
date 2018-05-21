1d32fdce00cc156c63e889e7fcbda54e2cefa71c
ltreum <ltreumer@web.de>
2018-05-21 18:20:48 +0200

    final changes and comments

--- php_includes/db_login.inc.php
+++ php_includes/db_login.inc.php
@@ -1,11 +1,11 @@
 <?php
 @ob_start();
 session_start();
-
+// if login button was submitted, check if login data are correct
 if (isset($_POST['submit'])) {
 
     include 'db_connect.inc.php';
-
+    //retrieve login data
     $ulog = mysqli_real_escape_string($conn, $_POST['ulog']);
     $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
@@ -15,22 +15,27 @@ if (isset($_POST['submit'])) {
         header("Location: ../index.php?login=empty");
         exit();
     } else {
-        
+        // if fields are not empty, create database query
         $sql = "SELECT * FROM person WHERE login='$ulog' OR email='$ulog'";
         $result = mysqli_query($conn, $sql);
+        // Check, if there is a user registered with login data in the database
         $resultCheck = mysqli_num_rows($result);
+        // if not, show error message
         if ($resultCheck < 1) {
             header("Location: ../index.php?login=loginfalse");
             exit();
         } else {
+            // if yes, compare the hashed password from database with the 
+            // hashed password from input form
             if ($row = mysqli_fetch_assoc($result)) {
                 echo $row['login'];
                 // de-heshing the password
                 $hashedPwdCheck = password_verify($pwd, $row['password']);
-                
+                // if passwords are not equal, show error message
                 if ($hashedPwdCheck == false) {
                     header("Location: ../index.php?login=pwderror");
                     exit();
+                 // if passwords macht, start the user session 
                 } elseif ($hashedPwdCheck == true) {
                     //log in the user here
                     session_start();
